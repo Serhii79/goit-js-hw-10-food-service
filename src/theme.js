ba3b4f157a279d1c5c516ff5e346c35c51e3d33d
Serhii Bondar <sergiibondar1979@gmail.com>
2020-12-16 10:07:53 +0200

    try to add checked

--- src/theme.js
+++ src/theme.js
@@ -9,7 +9,30 @@ const bodyRef = document.querySelector('body');
 bodyRef.classList.add(Theme.LIGHT);
 
 const toggleBackground = () => {
-    bodyRef.classList.toggle(Theme.DARK); 
+    bodyRef.classList.toggle(Theme.LIGHT);
+    bodyRef.classList.toggle(Theme.DARK);
+    // inputRef.removeAttribute("checked");
+    
+    // if (inputRef.checked !== true) {
+    //   inputRef.checked = true;
+    // } else {
+    //     inputRef.checked = false;
+    // };
+
+    // inputRef.setAttribute("checked", true);
 };
 
 inputRef.addEventListener('change', toggleBackground);
+
+// const checkedTheme = () => {
+//     if (bodyRef.Theme.DARK = true) {
+//         inputRef.setAttribute("checked", true);
+//     };
+// };
+// function change() {
+//     if (document.getElementById("check").checked !== true) {
+//       document.getElementById("check").checked = true;
+//     } else {
+//       document.getElementById("check").checked = false;
+//     }
+// }
