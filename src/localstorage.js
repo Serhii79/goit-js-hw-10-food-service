c9e9d6e86086bb44768e9151b74e344261ffb103
Serhii Bondar <sergiibondar1979@gmail.com>
2020-12-18 12:07:02 +0200

    Tired

--- src/localstorage.js
+++ src/localstorage.js
@@ -2,21 +2,20 @@ const refs = {
     input: document.querySelector('#theme-switch-toggle')
 }
 
-const savedMessage = localStorage.getItem('feedback-message');
+const savedMessage = localStorage.getItem('position-flag');
 
 const returnLocalStorage = () => {
     if (savedMessage) {
         refs.input.checked = savedMessage;
         console.log(refs.input.checked);
-    } else {
-        console.log('Hello');
-    }
+    };
 };
 
 returnLocalStorage();
+
 refs.input.addEventListener('input', handleInputChecked);
 
 function handleInputChecked() {
     const message = refs.input.checked;
-    localStorage.setItem('feedback-message', message);
+    localStorage.setItem('position-flag', message);
 }
