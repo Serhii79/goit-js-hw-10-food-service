ae6bb3d497762e1b0627f2223ace42fac2f5b875
Serhii Bondar <sergiibondar1979@gmail.com>
2020-12-24 08:47:44 +0200

    Try to save position of the flag

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,24 +1,45 @@
 const refs = {
-    body: document.querySelector('body')
+    body: document.querySelector('body'),
+    label: document.querySelector('.theme-switch__track'),
+    div: document.querySelector('.theme-switch__marker')
 };
-console.log(refs.body);
+// console.log(refs.body);
 
 const savedTheme = localStorage.getItem('variable-theme');
+const savedDiv = localStorage.getItem('position-div');
 
-const returnLocalStorage = () => {
+const returnThemeLocalStorage = () => {
     if (savedTheme) {
         refs.body.className = savedTheme;
         console.log(refs.body.className);
     } else {
-        console.log('Hello!');//Тимчасово для перевірки
+        console.log('Hello!');//Для перевірки
     };
 };
 
-returnLocalStorage();
+const returnDivLocalStorage = () => {
+    if (savedDiv) {
+        refs.div.checked = savedDiv;
+        console.log(refs.div.checked);
+    } else {
+        console.log('Hi!');//Для перевірки
+    };
+};
+
+
+returnThemeLocalStorage();
+returnDivLocalStorage();
 
 refs.body.addEventListener('change', handleBodyTheme);
+refs.div.addEventListener('change', handleDivChecked);
 
-function handleBodyTheme(event) {
+
+function handleBodyTheme() {
     const variableTheme = refs.body.className;
     localStorage.setItem('variable-theme', variableTheme);
-}
+};
+
+function handleDivChecked() {
+    const positionDiv = refs.div.checked;
+    localStorage.setItem('position-div', positionDiv);
+};
--- src/theme.js
+++ src/theme.js
@@ -5,60 +5,27 @@ const Theme = {
 
 const inputRef = document.querySelector('#theme-switch-toggle');
 const bodyRef = document.querySelector('body');
+// const divRef = document.querySelector('.theme-switch__marker');
 
-bodyRef.classList.add(Theme.LIGHT);
-
+// bodyRef.classList.add(Theme.LIGHT);
+// if (bodyRef.classList.contains(Theme.LIGHT) = true) {
+//     divRef.checked = true;
+// }
 const toggleBackground = () => {
-    // bodyRef.classList.toggle(Theme.LIGHT);
+    // if
+    //     (bodyRef.classList.contains(Theme.LIGHT) inputRef.checked) {
+        
+    // }
     // bodyRef.classList.toggle(Theme.DARK);
     // event.preventDefault();
 
     if (inputRef.getAttribute("checked") !== true) {
         bodyRef.classList.toggle(Theme.DARK);
         bodyRef.classList.toggle(Theme.LIGHT);
-        console.log(inputRef.checked);
     } else {
         bodyRef.classList.toggle(Theme.LIGHT);
         bodyRef.classList.toggle(Theme.DARK);
-        console.log(inputRef.checked);
     };
-
-    // if (inputRef.getAttribute("checked") === true) {
-    //     bodyRef.classList.toggle(Theme.LIGHT);
-    //     bodyRef.classList.toggle(Theme.DARK);
-    //     console.log(inputRef.checked);
-    // } else {
-    //     bodyRef.classList.toggle(Theme.DARK);
-    //     bodyRef.classList.toggle(Theme.LIGHT);
-    //     console.log(inputRef.checked);
-    // };
 };
 
 inputRef.addEventListener('change', toggleBackground);
-
-// const checkedTheme = () => {
-//     if (bodyRef.Theme.DARK = true) {
-//         inputRef.setAttribute("checked", true);
-//     };
-// };
-// function change() {
-//     if (document.getElementById("check").checked !== true) {
-//       document.getElementById("check").checked = true;
-//     } else {
-//       document.getElementById("check").checked = false;
-//     }
-// }
-
-// if (inputRef.checked === true) {
-    //     bodyRef.classList.remove(Theme.LIGHT);
-    //     bodyRef.classList.add(Theme.DARK);
-    // } else {
-    //     bodyRef.classList.remove(Theme.DARK);
-    //     bodyRef.classList.add(Theme.LIGHT);
-    // };
-
-    // if (inputRef.checked !== true) {
-    //   inputRef.checked = true;
-    // } else {
-    //     inputRef.checked = false;
-    // };
